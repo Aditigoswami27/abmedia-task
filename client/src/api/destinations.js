155da6770dadd5c02975df86a63556b846912dbf
Aditi Goswami <antra0045@gmail.com>
2025-05-27 14:16:21 +0530

    Update API base URL to Render backend

--- client/src/api/destinations.js
+++ client/src/api/destinations.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:5000'; // Use your backend URL
+const REACT_APP_API_BASE_URL = 'http://localhost:5000'; // Use your backend URL
 
 // Fetch all destinations from backend API
 export const fetchDestinations = async () => {
-  const res = await axios.get(`${BASE_URL}/api/destinations`);
+  const res = await axios.get(`${REACT_APP_API_BASE_URL}/api/destinations`);
   return res.data;
 };
